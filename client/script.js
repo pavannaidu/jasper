2c40c085f5613311f0437d84368d3b241c4a4cc1
Pavan Naidu <Pavan_Naidu@mckinsey.com>
2023-02-04 17:52:10 -0600

    update the server url

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     // fetch data from server -> bot's response
-    const response = await fetch('http://localhost:4000', {
+    const response = await fetch('https://jasper-emza.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
