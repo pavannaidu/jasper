4b1250e5d6f2e483efeda124c6032f202a0c11ea
Pavan Naidu <Pavan_Naidu@mckinsey.com>
2023-02-04 18:26:21 -0600

    restrict max-tokens to 200

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.post('/', async (req, res) => {
             model: "text-davinci-003",
             prompt: `${prompt}`,
             temperature: 0,
-            max_tokens: 2000,
+            max_tokens: 200,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
